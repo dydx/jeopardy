1082b6e0169236d53ea1935aebf84504fa162f48
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-12-14 15:04:09 -0500

    have core logic sort of working

--- build/js/main.js
+++ build/js/main.js
@@ -1,22 +1,54 @@
 'use strict'
 
-require('lovefield')
-require('./database.js')
-
-class Card {
-  constructor(category, number) {
-    this.category = category
-    this.number = number
-    this.question = ""
-    this.answers = []
-  }
-}
-
-class Category {
-  constructor(categoryName, categoryNumber) {
-    this.name = categoryName
-    this.number = categoryNumber
-    this.questions = []
+var questions = {
+  1 : {
+    'name': 'Addition',
+    'questions': {
+      1 : {
+        'text'    : 'What is 1 + 2?',
+        'value'   : 200,
+        'correct' : 2,
+        'choices' : [
+          '4',
+          '6',
+          '3',
+          '1'
+        ]
+      },
+      2 : {
+        'text'    : 'What is 55 + 17?',
+        'value'   : 400,
+        'correct' : 2,
+        'choices' : [
+          '78',
+          '89',
+          '72',
+          '62'
+        ]
+      },
+      3 : {
+        'text'    : 'What is 117 + -32?',
+        'value'   : 800,
+        'correct' : 1,
+        'choices' : [
+          '95',
+          '85',
+          '97',
+          '84'
+        ]
+      },
+      4 : {
+        'text'    : 'What is -51 + 2?',
+        'value'   : 1600,
+        'correct' : 1,
+        'choices' : [
+          '48',
+          '-49',
+          '54',
+          '-53'
+        ]
+      }
+    }
   }
 }
 
@@ -24,7 +56,17 @@ var cards = document.querySelectorAll('.card')
 
 for (var i = 0; i < cards.length; i++) {
   cards[i].addEventListener('click', function (event) {
-    this.classList.toggle('correct', 'incorrect')
+    var questionObject = questions[this.dataset.cat].questions[this.dataset.card]
+    var question = questionObject.text
+    var answer = questionObject.choices[questionObject.correct]
+    var reply = prompt(question)
+    if (answer == reply) {
+      this.classList.add('correct')
+    } else {
+      this.classList.add('incorrect')
+    }
+    this.removeEventListener('click')
   })
 }
+
 
