6e6508ab3d2678072fe11c5e5bcc272d9247c28c
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-12-14 09:26:47 -0500

    got the basic ui laid out

---
+++ Gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+
+gulp.task('sass', function () {
+  gulp.src('build/sass/main.scss')
+    .pipe(sass({
+      includePaths: require('node-bourbon').with(require('node-neat').includePaths)
+    }))
+    .pipe(gulp.dest('app/css/'))
+})
+
+gulp.task('javascript', function () {
+  gulp.src('build/js/main.js')
+    .pipe(gulp.dest('app/js/'))
+})
+
+gulp.task('default', ['sass', 'javascript'])
