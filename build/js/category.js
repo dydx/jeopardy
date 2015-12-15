54025e3dcc8b9eaec6c37e3f946e280d9c666b78
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-12-15 17:06:07 -0500

    got the Categories tested

--- build/js/category.js
+++ build/js/category.js
@@ -1,11 +1,18 @@
 'use strict'
 module.exports = class {
   constructor (name) {
+    if (typeof name !== 'string') {
+      throw new Error('category name must be a string')
+    }
+
     this.name = name
     this.questions = []
   }
 
   addQuestion (question) {
+    if (typeof question !== 'object') {
+      throw new Error('questions must be Question objects')
+    }
     this.questions.push(question)
     return this
   }
--- spec/categorySpec.js
+++ spec/categorySpec.js
@@ -1,9 +1,64 @@
 'use strict'
 var Category = require('../build/js/category')
+var Question = require('../build/js/question') 
 
 describe('Categories Class', function () {
-  it('runs a test successfully', function (done) {
-    expect(2).toEqual(2)
-    done()
+  describe('#new', function () {
+    it('returns a new Categories object when instantiated', function (done) {
+      let name = 'Testing'
+      let category = new Category(name)
+      expect(category).toEqual(jasmine.any(Category))
+      done()
+    })
+
+    it('throws an error when the incorrect number of arguments are used', function (done) {
+      expect(function () {
+        new Category()
+      }).toThrow()
+      done()
+    })
+  })
+
+  describe('.name', function () {
+    it('has a name value', function (done) {
+      let name = 'Testing'
+      let category = new Category(name)
+      expect(category.name).toEqual(name)
+      done()
+    })
+
+    it('rejects non-string values', function (done) {
+      let name = 331337
+      expect(function () {
+        new Category(name)
+      }).toThrow()
+      done()
+    })
+  })
+
+  describe('.addQuestion', function () {
+    it('starts with an empty questions array', function (done) {
+      let name = 'Testing'
+      let category = new Category(name)
+      expect(category.questions).toEqual([])
+      done()
+    })
+
+    it('can append new questions', function (done) {
+      let name = 'Testing'
+      let category = new Category(name)
+      category.addQuestion(new Question('a', 1, 'a'))
+      expect(category.questions.length).toEqual(1)
+      done()
+    })
+
+    it('does not allow incorrect question objects to be added', (done) => {
+      let name = 'Testing'
+      let category = new Category(name)
+      expect(function () {
+        category.addQuestion('whoa!')
+      }).toThrow()
+      done()
+    })
   })
 })
