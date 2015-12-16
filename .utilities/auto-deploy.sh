e486ba440d0b80cd2f9962ab7e351ba24e37b086
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-12-15 21:05:18 -0500

    this is killing me!

--- .utilities/auto-deploy.sh
+++ .utilities/auto-deploy.sh
@@ -6,11 +6,10 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/dydx/jeopardy gh-pages > /dev/null
 
-cd gh-pages
-git rm -rf ./
-cp -Rf $HOME/* ./
 git add -f .
 git commit -m "Latest production deploy from $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+git checkout gh-pages
+git merge master
 git push -fq origin gh-pages > /dev/null
 
 echo -e "Production is deployed"
