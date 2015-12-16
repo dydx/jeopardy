18c65a708463d9b0066630d0819564b5e010e8ca
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-12-15 22:58:44 -0500

    I hope this works!

--- .utilities/auto-deploy.sh
+++ .utilities/auto-deploy.sh
@@ -13,9 +13,16 @@ echo -e "Deploying to production... "
 # commit with build ID
 # force push
 
+echo -e " -> making app directory"
 mkdir app
+
+echo -e " -> copying index.html into app"
 mv index.html app/index.html
+
+echo -e " -> compiling javascript and sass into app"
 npm build # this compiles the contents of build into app
+
+echo -e " -> setting up deploy repo"
 cd app
 git init
 
@@ -23,8 +30,11 @@ git config user.name "Travis CI"
 git config user.email "joshua.sandlin@gmail.com"
 
 git add .
+
+echo -e " -> committing app deployment"
 git commit -m "Deploy to GitHub Pages"
 
+echo -e " -> pushing app deployment"
 git push --forge --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 
-echo -e "Production app is deployed"
+echo -e "Production app is deployed (hopefully!)"
