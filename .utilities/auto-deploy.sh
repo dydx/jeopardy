ee971a457b7bf3ef2a9ff533dd6dfb83ebab55f7
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-12-15 20:41:37 -0500

    this is weird

--- .utilities/auto-deploy.sh
+++ .utilities/auto-deploy.sh
@@ -7,8 +7,8 @@ git config --global user.name "travis-ci"
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/dydx/jeopardy gh-pages > /dev/null
 
 cd gh-pages
-git rm -rf ./javadoc
-cp -Rf $HOME/javadoc-latest ./javadoc
+git rm -rf ./
+cp -Rf $HOME/* ./
 git add -f .
 git commit -m "Latest production deploy from $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
